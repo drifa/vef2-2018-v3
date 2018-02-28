1ebb9ecb49c3669fd30942877c98997661ba180a
Drífa Sóley Heimisdóttir <drifasoley@Drifas-MacBook-Pro.local>
2018-02-28 23:50:39 +0000

    test

--- database.js
+++ database.js
@@ -5,9 +5,14 @@ const connectionString = process.env.DATABASE_URL// + '?ssh=true';
 exports.runSQL = async(query, params) => {
   const client = new Client({ connectionString });
 
+  const query = {
+    text: query,
+    values: params
+  };
+
   await client.connect();
   try {
-    const result = await client.query(query, params);
+    const result = await client.query(query);
     return result.rows;
   } catch (err) {
     throw err;
